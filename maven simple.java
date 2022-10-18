498af32f560c40245c1716de265a62f01276afc3
AlejandroVelazR <115710108+AlejandroVelazR@users.noreply.github.com>
2022-10-18 12:26:07 +0200

    Add files via upload

---
+++ maven simple.java
@@ -0,0 +1,36 @@
+package mavensimple;
+
+
+public class Metodos {
+
+	public Metodos(){
+		//sumaNumeros();
+		//sumaNumeros(10.0f,30.0f);
+		//float result = divisionNumerosReturn();
+		//System.out.println(result);
+		//String leyenda = divisionNumerosReturn(34.0f,40.0f,"El resultado de la division es:")
+		//System.out.println(leyenda);
+	}
+	
+	public void sumaNumeros(){
+		float resultado;
+		float a = 20.0f;
+		float b = 10.0f;
+		resultado = a + b;
+		System.out.println(resultado);
+	}
+	
+	public void sumaNumeros(float a,float b){
+		float resultado;
+		resultado = a + b;
+		System.out.printin(resultado);
+	}
+	
+	public float divisionNumerosReturn()}
+		float a = 100.5f;
+		float b = 20.1f;
+		float resultado;
+		resultado = a / b;
+	}
+	
+}
